dd08abbd8478e4dc44f7429a2d4ddf1da591df77
Grib <grib.ua@ukr.net>
2024-01-30 20:33:47 +0000

    just save

---
+++ src/op-loops.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//Operators
+let a = 20;
+let b = 30;
+// a = a + 2;
+a += 2;
+a /= 11;
+a *= 10;
+// a %= 10;
+a **= 3;
+
+// b = b + 1
+// b += 1;
+// b++;
+// b--;
+// ++b;
+// --b;
+
+console.log(50 - ++b);
+console.log(b)
+console.log(50 - --b);
+console.log(b)

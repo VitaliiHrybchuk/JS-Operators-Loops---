6259d898b5aa1fd07bf8010b0f752fc28446c20d
Grib <grib.ua@ukr.net>
2024-01-31 14:21:01 +0000

    save

--- src/op-loops.js
+++ src/op-loops.js
@@ -1,23 +1,87 @@
 'use strict';
 
-//Operators
-let a = 20;
-let b = 30;
-// a = a + 2;
-a += 2;
-a /= 11;
-a *= 10;
-// a %= 10;
-a **= 3;
+// //Operators
+// let a = 20;
+// let b = 30;
+// // a = a + 2;
+// a += 2;
+// a /= 11;
+// a *= 10;
+// // a %= 10;
+// a **= 3;
 
-// b = b + 1
+// // b = b + 1
 // b += 1;
 // b++;
 // b--;
 // ++b;
 // --b;
 
-console.log(50 - ++b);
-console.log(b)
-console.log(50 - --b);
-console.log(b)
+// console.log(50 - ++b);
+// console.log(b)
+// console.log(50 - --b);
+// console.log(b)
+// let ms = 60;
+// let kms = 50;
+// let myRate = 40;
+// // if (myRate >= kms || myRate >= ms ){
+// //   console.log('I am a kms');
+// //   }else if (myRate >= ms){
+// //     console.log('I am a ms');
+// //   }else{
+// //     console.log('I am a just sportsman');
+//   // }
+// let res = myRate >= kms || myRate >= ms
+//                   ?console.log('I am a ms')
+//                   :console.log('I am a just sportsman')
+//   let result = myRate >= kms || myRate >= ms || kms > ms;
+//   // console.log(result);
+
+// let c = 0;
+// console.log(c || 10);
+// console.log(c ?? 20);
+
+// let year = Number(prompt('Enter year, pls'));
+// console.log(year);
+// // console.log(isNaN)
+// if (Number.isNaN(year)) {
+//   console.log('Entered wrong data');
+// } else if (year % 100 === 0) {
+//   if (year % 400 === 0) {
+//     console.log('It is a leap year');
+//   }else{
+//     console.log('It is not a leap year');
+//   }
+// }else if(year % 4 === 0 && year > 4){
+//
+// }else{
+//   console.log('It is not a leap year');
+// }
+// if (Number.isNaN(year)) {
+//   console.log('Entered wrong data');
+// } else {
+//   year % 4 === 0 &&
+//     (year % 100 !== 0 || year % 400 === 0) &&
+//     year > 4 &&
+//     console.log('It is a leap year');
+
+//   (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0) && year > 4) ||
+//     console.log('It is not a leap year');
+// }
+
+// let count = 12;
+// while(count < 10){
+//   console.log(count);
+//   count++;
+// }
+
+// do{
+//   console.log(count);
+//   count--;
+// }while(count < 10);
+let i = 1;
+for(; ; ){
+  console.log(`$(i * 2)`);
+  i++;
+  if(i >= 10) break;
+}
